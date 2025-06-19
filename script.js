34208c8d440d07dd78a6f2da9b2cbac37c2c247f
extrememine1 <extremsch@gmail.com>
2025-06-19 21:01:51 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Typewriter - Credits to Cheng Hsuan*/
+const words = [
+  "Macro Developer",
+  "UI/UX Designer",
+  "Game Developer",
+  "Coding Enthusiast",
+  "Gamer",
+  "Student Leader",
+  "Publications Officer"
+];
+const el = document.getElementById("typewriter");
+let wordIndex = 0;
+let letterIndex = 0;
+let isDeleting = false;
+
+function typeLoop() {
+  const currentWord = words[wordIndex];
+  const visibleText = currentWord.substring(0, letterIndex);
+  el.textContent = visibleText;
+
+  if (!isDeleting && letterIndex < currentWord.length) {
+    letterIndex++;
+    setTimeout(typeLoop, 60);
+  } else if (isDeleting && letterIndex > 0) {
+    letterIndex--;
+    setTimeout(typeLoop, 30);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) {
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+    setTimeout(typeLoop, 1000);
+  }
+}
+
+window.onload = typeLoop;
